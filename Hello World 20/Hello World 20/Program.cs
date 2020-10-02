b6e8d5f18130c47009631a83004cf0e0a5ca59ba
la2697 <56022392+la2697@users.noreply.github.com>
2020-10-02 09:53:41 +0200

    Hello World Programm

---
+++ Hello World 20/Hello World 20/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Moin meine HSP Kollegen");
+            Console.ReadKey();
+        }
+    }
+}
